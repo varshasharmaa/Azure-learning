a1db8a21eb5174f26cb40f387e2903ad34e0d2e1
varshasharmaa <72292543+varshasharmaa@users.noreply.github.com>
2020-10-03 21:10:34 +0530

    Delete read-dbfs-file.py

--- read-dbfs-file.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-#read file from DBFS folder
-df = spark.read.csv("/FileStore/tables/1800.csv").toDF("a","b","c","d","e","f","g","h")
-df.show()
-
-# COMMAND ----------
-
