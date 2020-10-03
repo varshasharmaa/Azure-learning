d372bb867f75fd0c65fa2cb172a1de00ab814ccd
varshasharmaa <72292543+varshasharmaa@users.noreply.github.com>
2020-10-03 21:49:46 +0530

    Update: README.md pyspark-notebook

---
+++ pyspark-notebook/max-temperatures.py
@@ -0,0 +1,20 @@
+# Databricks notebook source
+# to find out the max temperature by id
+def parseLine(line):
+    fields = line.split(',')
+    stationID = fields[0]
+    entryType = fields[2]
+    temperature = float(fields[3]) * 0.1 * (9.0 / 5.0) + 32.0
+    return (stationID, entryType, temperature)
+
+lines = sc.textFile("FileStore/tables/1800.csv")
+parsedLines = lines.map(parseLine)
+maxTemps = parsedLines.filter(lambda x: "TMAX" in x[1])
+stationTemps = maxTemps.map(lambda x: (x[0], x[2]))
+maxTemps = stationTemps.reduceByKey(lambda x, y: max(x,y))
+results = maxTemps.collect()
+for result in results:
+    print(result[0] + "\t{:.2f}F".format(result[1]))
+
+# COMMAND ----------
+
